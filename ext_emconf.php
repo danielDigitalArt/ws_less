48513df6c287e03dfe683b248e4add218d247f7d
Daniel Li <dli@digital-art.de>
2019-01-08 17:26:00 +0100

    Typo3 9 compatibility changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.1.0-7.6.99',
+			'typo3' => '8.0.0-9.9.99',
 		),
 		'conflicts' => array(
 		),
